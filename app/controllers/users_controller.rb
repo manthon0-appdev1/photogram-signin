b208b9b15c7e3cf25cdc712294577733fc598503
manthon0 <manthon0@chicagobooth.edu>
2022-11-19 00:11:56 +0000

    login logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+
+  def new_session_form
+    render({:template => "users/signin_form.html.erb"})
+  end
+  def authenticate
+    un = params.fetch("input_username")
+    pw = params.fetch("input_password")
+
+    user = User.where({:username => un}).first
+
+    if user == nil
+      redirect_to("/user_sign_in", {:alert => "No one"})
+    else
+      if user.authenticate(pw)
+        session.store(:user_id, user.id)
+      redirect_to("/")
+      else
+        redirect_to("/user_sign_in")
+      end
+    end
+  end
+  def new_registration_form
+    render({:template => "users/signup_form.html.erb"})
+  end
   def index
     @users = User.all.order({ :username => :asc })
 
@@ -16,12 +40,21 @@ class UsersController < ApplicationController
     user = User.new
 
     user.username = params.fetch("input_username")
-
-    user.save
-
-    redirect_to("/users/#{user.username}")
+    user.password = params.fetch("input_password")
+    user.password_confirmation = params.fetch("input_password_confirmation")
+    save_status = user.save
+    if save_status == true
+      session.store(:user_id, user.id)
+      redirect_to("/users/#{user.username}", {:notice => "Welcome"})    
+    else
+      redirect_to("/user_sign_up", {:alert => user.errors.full_messages.to_sentence})
+    end
   end
 
+  def toast_cookies
+    reset_session
+    redirect_to("/")
+  end
   def update
     the_id = params.fetch("the_user_id")
     user = User.where({ :id => the_id }).at(0)
